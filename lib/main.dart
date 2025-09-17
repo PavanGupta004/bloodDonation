bc5d7a6d4f055b8a22581d946bafcbef5cfa400d
Vishhal <101860494+vishhalnarkar@users.noreply.github.com>
2025-09-17 09:56:39 +0530

    Merge

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'SOS Blood Donation',
       theme: ThemeData(primarySwatch: Colors.red),
-      home: HomePage(),
+      home: SelectSignUp(),
       // StreamBuilder<User?>(
       //   stream: FirebaseAuth.instance.authStateChanges(),
       //   builder: (context, snapshot) {
